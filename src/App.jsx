74ed7e9a28b1ca96d3b703ea3bd866899fa894fd
David Franklin D. <davidfranklin1999@gmail.com>
2025-08-28 17:26:27 +0530

    feature: add dashboard feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import PrivateRoute from './routes/PrivateRoutes'
+import Sidebar from './components/layouts/Sidebar'
+import DashboardPage from './pages/DashboardPage'
 
 function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<h1>Good</h1>} />
+      <Route path="/" element={<DashboardPage />} />
       <Route element={<PrivateRoute roles={['admin']} />}>
         
       </Route>
