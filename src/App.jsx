01ec2a2c45b41a012a118221d22c5614bc5e7da9
David Franklin D. <davidfranklin1999@gmail.com>
2025-08-30 11:31:51 +0530

    chore: change the route url and remove unnecessory files

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,16 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import PrivateRoute from './routes/PrivateRoutes'
-import Sidebar from './components/layouts/Sidebar'
 import DashboardPage from './pages/DashboardPage'
-import TextEditor from './components/editor/TextEditor'
 import CreateBlog from './components/creatBlog'
 
 function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<DashboardPage />} />
+      <Route path="/user" element={<DashboardPage />} />
       <Route path="/creatblog" element={<CreateBlog />} />
+      
       <Route element={<PrivateRoute roles={['admin']} />}>
         
       </Route>
